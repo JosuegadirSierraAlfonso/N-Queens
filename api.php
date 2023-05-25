ae6cc11987afaf821c57bdcf2c56d37e49f20725
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-05-25 15:10:15 -0500

    feat: :rocket: N-Queens PHP
    the logic was passed to php

---
+++ api.php
@@ -0,0 +1,71 @@
+<?php
+
+function solveNQueens($n) {
+    if ($n === 1) return [["Q"]];
+
+    $col = [];
+    $posDiag = [];
+    $negDiag = [];
+
+    $res = [];
+    $board = array_fill(0, $n, array_fill(0, $n, "."));
+
+    // Helper functions
+
+    $isValid = function($r, $c) use (&$col, &$posDiag, &$negDiag) {
+        return !isset($col[$c]) && !isset($posDiag[$r + $c]) && !isset($negDiag[$r - $c]);
+    };
+
+    $addQueen = function($r, $c) use (&$col, &$posDiag, &$negDiag, &$board) {
+        $col[$c] = true;
+        $posDiag[$r + $c] = true;
+        $negDiag[$r - $c] = true;
+        $board[$r][$c] = "Q";
+    };
+
+    $removeQueen = function($r, $c) use (&$col, &$posDiag, &$negDiag, &$board) {
+        unset($col[$c]);
+        unset($posDiag[$r + $c]);
+        unset($negDiag[$r - $c]);
+        $board[$r][$c] = ".";
+        
+    };
+
+    // Recursive backtracking function
+
+    $recurse = function($row) use ($n, &$res, &$board, $isValid, $addQueen, $removeQueen, &$recurse) {
+        // Base case
+        if ($row === $n) {
+            $res[] = array_map(function($row) {
+                return implode("", $row);
+            }, $board);
+            return;
+        }
+
+        // Recurrence relation
+        for ($col = 0; $col < $n; $col++) {
+            if ($isValid($row, $col)) {
+                $addQueen($row, $col);
+                // Recurse
+                $recurse($row + 1);
+                // Backtrack
+                $removeQueen($row, $col);
+            }
+        }
+    };
+
+    $recurse(0);
+    return $res;
+}
+
+// Imprimir la respuesta en pantalla
+$n = 8; // Cambiar el valor de "n" según sea necesario
+$solutions = solveNQueens($n);
+echo "Soluciones para $n reinas:<br>";
+foreach ($solutions as $index => $solution) {
+    echo "Solución " . ($index + 1) . ":<br>";
+    foreach ($solution as $row) {
+        echo $row . "<br>";
+    }
+}
+?>
